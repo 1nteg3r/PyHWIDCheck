300bd87ed41ec9d4e4422d40a3be5006bbbc1ddb
1nteg3r <54937634+1nteg3r@users.noreply.github.com>
2021-04-06 08:55:23 +0200

    Create HWID-CHECK.py

---
+++ HWID-CHECK.py
@@ -0,0 +1,14 @@
+import requests
+import subprocess
+def HWID():
+    cmd = 'wmic csproduct get uuid'
+    uuid = str(subprocess.check_output(cmd))
+    pos1 = uuid.find("\\n")+2
+    uuid = uuid[pos1:-15]
+    return uuid
+print(f"Tvojot HWID e {HWID()} ")
+hwid_auth = requests.get("RAW PASTEBIN LINK WITH THE HWIDS ADDED")
+if HWID() in hwid_auth.text:
+    print("Welcome")
+else:
+    print("Your HWID was not found in our database!")
