e05c7ca4c7e4f0103e4faf4cae78c80bbaee853d
1nteg3r <54937634+1nteg3r@users.noreply.github.com>
2021-04-06 08:57:31 +0200

    Update HWID-CHECK.py

--- HWID-CHECK.py
+++ HWID-CHECK.py
@@ -6,7 +6,7 @@ def HWID():
     pos1 = uuid.find("\\n")+2
     uuid = uuid[pos1:-15]
     return uuid
-print(f"Tvojot HWID e {HWID()} ")
+print(f"Your HWID is {HWID()} ")
 hwid_auth = requests.get("RAW PASTEBIN LINK WITH THE HWIDS ADDED")
 if HWID() in hwid_auth.text:
     print("Welcome")
